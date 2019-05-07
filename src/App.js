0e1e9520e2986e4a167d8263a1182bb9e118c9f9
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-05-07 12:44:42 -0500

    Adding web3 connector through web3-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+import { useWeb3Context } from "web3-react";
+import Button from '@material-ui/core/Button';
 import './App.css';
+
+
+function MyComponent() {
+  const context = useWeb3Context();
+
+  useEffect(() => {
+    context.account || context.unsetConnector("MetaMask")
+  }, [ context.account ]);
+
+  return (
+    context.account ? (
+      <Fragment>
+        <h1>Account info </h1>
+        <p>{ context.account }</p>
+      </Fragment>
+    ) : <h1> No account </h1>
+  );
+}
+
+function MetaMaskComponent() {
+  const context = useWeb3Context();
+
+  return (context.active ? <MyComponent /> : 
+    <Fragment>
+      <h1> Connnect to </h1>
+      <Button 
+        variant="contained" 
+        color="primary"
+        onClick={() => context.setConnector("MetaMask")}
+      >
+        MetaMask
+      </Button>
+    </Fragment>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MetaMaskComponent />
       </header>
     </div>
   );
